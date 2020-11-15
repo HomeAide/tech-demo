337dec1170f29295e6d5f15b034479513beb6e80
kovec681 <sethborkovec@gmail.com>
2020-11-15 14:26:24 -0700

    User management

--- www/demo/urls.py
+++ www/demo/urls.py
@@ -4,4 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('add_device', views.add_device, name='add_device'),
+    path('create_user', views.create_user, name='create_user'),
+    path('signin', views.signin, name='signin'),
+    path('signout', views.signout, name='signout'),
 ]
--- www/demo/views.py
+++ www/demo/views.py
@@ -1,29 +1,26 @@
 from django.shortcuts import render, redirect
-from django.core.exceptions import PermissionDenied
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
 from .models import AT
 
 # Create your views here.
 def index(request):
+    if not request.user.is_authenticated:
+        return redirect('signin')
+
     context = {}
-    message = "Login"
-    at_devices = None
 
-    if request.user.is_authenticated:
-        message = "Success"
-        context['user'] = request.user
-        at_devices = AT.objects.all()
-
+    context['user'] = request.user
+    at_devices = AT.objects.all()
     context['at_devices'] = at_devices
-    context['message'] = message
+
     template = 'demo/index.html'
 
     return render(request, template, context)
 
 def add_device(request):
-
     if not request.user.is_authenticated:
-        # return redirect('403')
-        raise PermissionDenied
+        return redirect('signin')
 
     if request.method == 'POST':
         new_entry = AT(name=request.POST['name'], description=request.POST['description'], location=request.POST['location'])
@@ -34,3 +31,50 @@ def add_device(request):
     template = 'demo/add.html'
 
     return render(request, template, context)
+
+def create_user(request):
+    if not request.user.is_authenticated:
+        return redirect('signin')
+
+    new_user = ""
+    password = ""
+
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+
+        if form.is_valid():
+            form.save()
+            # username = form.cleaned_data['username']
+            # password = form.cleaned_data['password1']
+            # user = authenticate(username=username, password=password)
+            # login(request, user)
+            new_user = form.cleaned_data['username']
+            password = form.cleaned_data['password1']
+    else:
+        form = UserCreationForm()
+
+    context = {'form': form, 'new_user': new_user, 'password': password}
+    template = 'demo/create_user.html'
+
+    return render(request, template, context)
+
+
+def signin(request):
+    if request.user.is_authenticated:
+        return redirect('/')
+
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            return redirect('/')
+
+    form = AuthenticationForm()
+    return render(request, 'demo/login.html', {'form': form})
+
+def signout(request):
+    logout(request)
+    return redirect('signin')
